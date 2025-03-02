0487c9cc304578679ffa7b7d7c688406ce523f52
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-03-02 17:52:32 +0700

    แก้ไขโค้ด 01-06

--- study05.js
+++ study05.js
@@ -2,12 +2,14 @@
 // คือ เครื่องหมายที่ใช้ในการดำเนินการกับข้อมูล
 //------------------------------
 // Arithmetic Operators + - * / % ++ -- **
-
+console.log(10 ** 2)
+console.log(50 % 7)
 
 // Comparison Operators == === != !== > < >= <=
+console.log(10 == '10') //true ไม่สนใจ type
+console.log(10 === '10') //false สนใจ type
 
-
-// Logical Operators && || !
+// Logical Operators &&  ||  !
 
 
 
